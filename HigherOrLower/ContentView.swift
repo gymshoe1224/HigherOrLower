6230d8f404af38e08914e4f9ea3eb98230a8e3d1
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2022-11-14 11:45:13 -0600

    added deck of card images and start screen

--- HigherOrLower/ContentView.swift
+++ HigherOrLower/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Higher Or Lower")
+                .padding()
+                .font(.largeTitle)
+            Image("red_back")
+                .resizable()
+                .frame(width: 256, height: 391)
         }
         .padding()
     }
