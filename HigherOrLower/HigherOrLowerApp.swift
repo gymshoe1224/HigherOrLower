de705960b78c9ae4f441d6eb3da9d07596e9277d
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2022-11-14 10:30:48 -0600

    Initial Commit

---
+++ HigherOrLower/HigherOrLowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HigherOrLowerApp.swift
+//  HigherOrLower
+//
+//  Created by Chris Markiewicz on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct HigherOrLowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
