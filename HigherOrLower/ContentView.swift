7d9698a1a8ef63f7608b3f0479b54f44f41f0a11
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2022-11-15 14:17:44 -0600

    ad

--- HigherOrLower/ContentView.swift
+++ HigherOrLower/ContentView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = Dictionary<Int, String>()
+    @State private var shuffledCards = [Int]()
+    @State private var score = 0
+    @State private var currentCard = "red_back"
     var body: some View {
-        VStack {
-            Text("Higher Or Lower")
-                .padding()
-                .font(.largeTitle)
-            Image("red_back")
-                .resizable()
-                .frame(width: 256, height: 391)
+        ZStack {
+            VStack {
+                Text("Higher Or Lower")
+                    .padding()
+                    .font(.largeTitle)
+                Image("\(currentCard)")
+                    .resizable()
+                    .frame(width: 256, height: 391)
+                    .onTapGesture {
+                        if currentCard == "red_back" {
+                            addCards()
+                            currentCard = cards[shuffledCards[0]]!
+                        }
+                    }
+                Button("Reset") {
+                    shuffledCards = cards.keys.shuffled()
+                }
+            }
         }
-        .padding()
+    }
+    func addCards()
+    {
+        if  let directory = Bundle.main.path(forResource: "PNG", ofType: ""),
+             let files = try? FileManager.default.contentsOfDirectory(atPath: directory) {
+             for file in files {
+                 var i = 0
+                 cards[i] = file
+                 i+=1
+             }
+         }
     }
 }
 
